d9721b7230587e9a62404fbcd5001c2cd0e29240
Abner Silva <abner.barbosa@sou.inteli.edu.br>
2022-11-15 10:30:55 -0300

    New Template next.ts

---
+++ stories/index.stories.tsx
@@ -0,0 +1,13 @@
+import index from '../pages/index';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Example/index',
+  component: index,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {},
+  args:{},
+}
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+export const index_stori = {}

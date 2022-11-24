b946d79ba5331f2b62a221b1c755909f4832cf75
Abner Silva <abner.barbosa@sou.inteli.edu.br>
2022-11-24 11:44:03 -0300

    FEAT: new features and configures

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
+import React from "react";
+import Hello from "../components/Hello";
+
 export default function Home() {
-  return (
-    <div className="bg-black text-center">
-      <h1 className="text-cyan-200">Oi</h1>
-    </div>
-  )
+	return (
+		<div className="bg-black text-center">
+			<h1 className="text-cyan-200">Oi</h1>
+			<Hello/>
+		</div>
+	);
 }
